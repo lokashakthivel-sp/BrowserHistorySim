530da2cbca52f19c8f4548804bf07a2fa14fc297
lokashakthivel-sp <lokashakthivelsp@gmail.com>
2025-08-27 13:40:41 +0530

    FileManager implementation completed

--- include/FileManager.h
+++ include/FileManager.h
@@ -3,10 +3,11 @@
 #include <vector>
 #include <string>
 
-// to save and load history of each tab from separate
+// to save and load history of each tab from separate files
 class FileManager
 {
 public:
-    void saveHistory(std::vector<Tab *> &tabs);
-    void loadHistory(std::vector<Tab *> &tabs);
+    // set to static so it can be accessed without object
+    static void saveHistory(std::vector<Tab *> &tabs);
+    static void loadHistory(std::vector<Tab *> &tabs);
 };
--- src/FileManager.cpp
+++ src/FileManager.cpp
@@ -3,10 +3,50 @@
 #include "FileManager.h"
 using namespace std;
 
+// filename is of format history/tab1.txt
+
 void FileManager::saveHistory(vector<Tab *> &tabs)
 {
+    for (int i = 0; i < tabs.size(); i++)
+    {
+        string filename = "history/tab" + to_string(i + 1) + ".txt";
+        ofstream file(filename);
+        if (!file)
+        {
+            cout << "Could not open " << filename << endl;
+            return;
+        }
+        HistoryNode *temp = tabs[i]->getHistoryHead();
+        // writing history DLL into file
+        while (temp)
+        {
+            file << temp->url << "\n";
+            temp = temp->next;
+        }
+        file.close();
+    }
 }
 
 void FileManager::loadHistory(vector<Tab *> &tabs)
 {
+    for (int i = 0; i < tabs.size(); i++)
+    {
+        string filename = "history/tab" + to_string(i + 1) + ".txt";
+        ifstream file(filename);
+        if (!file)
+        {
+            cout << "No history stored" << filename << endl;
+            return;
+        }
+        string url;
+        // reading history from file
+        while (getline(file, url))
+        {
+            if (!url.empty())
+            {
+                tabs[i]->visit(url);
+            }
+        }
+        file.close();
+    }
 }
